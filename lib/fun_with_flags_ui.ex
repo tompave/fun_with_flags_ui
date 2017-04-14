1fe08eda792ccf8cf11748acd5f1da2295f5c6f7
tom <tommaso@pavese.me>
2017-04-14 14:47:38 +0100

    new project

---
+++ lib/fun_with_flags_ui.ex
@@ -0,0 +1,18 @@
+defmodule FunWithFlagsUi do
+  @moduledoc """
+  Documentation for FunWithFlagsUi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FunWithFlagsUi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fun_with_flags_ui_test.exs
@@ -0,0 +1,8 @@
+defmodule FunWithFlagsUiTest do
+  use ExUnit.Case
+  doctest FunWithFlagsUi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end

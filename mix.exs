1729f6fbee9169bbf4561dca2e7549ca2fe71454
tom <tommaso@pavese.me>
2017-04-27 00:35:34 +0100

    added ex_doc package

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule FunWithFlagsUi.Mixfile do
       {:plug, "~> 1.3.5"},
       {:cowboy, "~> 1.1", optional: true},
       {:fun_with_flags, "~> 0.7.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
 

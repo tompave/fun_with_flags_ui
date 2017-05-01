a3756e7c65f612b121297e804b739c25ea1f0f4a
tom <tommaso@pavese.me>
2017-05-01 02:19:55 +0100

    more template tests

--- test/fun_with_flags/ui/templates_test.exs
+++ test/fun_with_flags/ui/templates_test.exs
@@ -2,7 +2,7 @@ defmodule FunWithFlags.UI.TemplatesTest do
   use ExUnit.Case, async: true
 
   alias FunWithFlags.UI.Templates
-  # alias FunWithFlags.{Flag, Gate}
+  alias FunWithFlags.{Flag, Gate}
 
   import FunWithFlags.UI.TestUtils
 
@@ -27,6 +27,30 @@ defmodule FunWithFlags.UI.TemplatesTest do
       out = Templates._head(conn: conn, title: "Coconut")
       assert String.contains?(out, "<title>FunWithFlags - Coconut</title>")
       assert String.contains?(out, ~s{href="/pear/assets/style.css"})
+    end
+  end
+
+
+  describe "index()" do
+    setup do
+      flags = [
+        %Flag{name: :pineapple, gates: [Gate.new(:boolean, true)]},
+        %Flag{name: :papaya, gates: [Gate.new(:boolean, false)]},
+      ]
+      {:ok, flags: flags}
+    end
+
+    test "it renders", %{conn: conn, flags: flags} do
+      out = Templates.index(conn: conn, flags: flags)
+      assert is_binary(out)
+    end
+
+    test "it includes the right content", %{conn: conn, flags: flags} do
+      out = Templates.index(conn: conn, flags: flags)
+      assert String.contains?(out, "<title>FunWithFlags - List</title>")
+      assert String.contains?(out, ~s{<a href="/pear/new" class="btn btn-secondary">New Flag</a>})
+      assert String.contains?(out, ~s{<a href="/pear/flags/pineapple">pineapple</a>})
+      assert String.contains?(out, ~s{<a href="/pear/flags/papaya">papaya</a>})
     end
   end
 end

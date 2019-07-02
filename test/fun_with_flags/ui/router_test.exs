f5ed298f32239b88f6db6d772da36c7f4256fcfd
Benjamin Milde <benni@kobrakai.de>
2019-07-02 09:51:18 +0200

    Add script name support using minimal change

--- test/fun_with_flags/ui/router_test.exs
+++ test/fun_with_flags/ui/router_test.exs
@@ -18,6 +18,18 @@ defmodule FunWithFlags.UI.RouterTest do
 
   @opts Router.init([])
 
+  describe "script_name" do
+    test "redirects to /pear/flags" do
+      conn =
+        conn(:get, "/")
+        |> Map.put(:script_name, ["pear"])
+        |> Router.call(@opts)
+
+      assert 302 = conn.status
+      assert ["/pear/flags"] = get_resp_header(conn, "location")
+    end
+  end
+
   describe "GET /" do
     test "redirects to /flags" do
       conn = request!(:get, "/")
@@ -112,7 +124,7 @@ defmodule FunWithFlags.UI.RouterTest do
   describe "DELETE /flags/:name/boolean" do
     test "when the flag exists, it deletes its boolean gate and redirects to the flag page" do
       {:ok, true} = FunWithFlags.enable :frozen_yogurt
-      {:ok, true} = FunWithFlags.enable :frozen_yogurt, for_group: "some_group" 
+      {:ok, true} = FunWithFlags.enable :frozen_yogurt, for_group: "some_group"
 
       assert %Flag{name: :frozen_yogurt, gates: [%Gate{type: :boolean}, %Gate{type: :group}]} = FunWithFlags.get_flag(:frozen_yogurt)
 
@@ -128,7 +140,7 @@ defmodule FunWithFlags.UI.RouterTest do
   describe "DELETE /flags/:name/percentage" do
     test "when the flag exists, it deletes its current percentage gate and redirects to the flag page" do
       {:ok, true} = FunWithFlags.enable :pizza, for_percentage_of: {:time, 0.5}
-      {:ok, true} = FunWithFlags.enable :pizza, for_group: "some_group" 
+      {:ok, true} = FunWithFlags.enable :pizza, for_group: "some_group"
 
       assert %Flag{name: :pizza, gates: [%Gate{type: :percentage_of_time, for: 0.5}, %Gate{type: :group}]} = FunWithFlags.get_flag(:pizza)
 

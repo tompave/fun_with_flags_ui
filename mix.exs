5dedfbe169918f4ecfcbb0720f4421904a8ad771
tom <tommaso@pavese.me>
2022-03-20 12:05:02 +0100

    Require more recent version of redix

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule FunWithFlagsUi.Mixfile do
       {:plug_cowboy, ">= 2.0.0", optional: true},
       {:cowboy, ">= 2.0.0", optional: true},
       {:fun_with_flags, "~> 1.1"},
-      {:redix, ">= 0.8.2", only: [:dev, :test]},
+      {:redix, "~> 1.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:credo, "~> 1.6", only: :dev, runtime: false},
     ]

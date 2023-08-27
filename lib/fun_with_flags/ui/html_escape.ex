b29227e93337393d46e1b94d187361890bd4353e
tom <tommaso@pavese.me>
2023-08-27 15:45:52 +0200

    Rename "html_safe" as "html_escape", because it matches Phoenix.HTML.
    We're copying (and basically vendoring) code from the phoenix_html package. Using the same function names has some advantages:
    1. It's tidier.
    2. It's easier to switch to Phoenix.HTML in the future if we choose to.
    3. It's easier to backport fixes from upstream.

--- lib/fun_with_flags/ui/html_escape.ex
+++ lib/fun_with_flags/ui/html_escape.ex
@@ -4,14 +4,14 @@ defmodule FunWithFlags.UI.HTMLEscape do
   # This module contains HTML sanitization code copied from Phoenix.HTML.
   # See: https://github.com/phoenixframework/phoenix_html/blob/v3.3.1/lib/phoenix_html/engine.ex#L24
 
-  def html_safe(val) when is_binary(val) do
-    html_safe(val, 0, val, [])
+  def html_escape(bin) when is_binary(bin) do
+    html_escape(bin, 0, bin, [])
   end
 
-  def html_safe(val) do
+  def html_escape(val) do
     val
     |> to_string()
-    |> html_safe()
+    |> html_escape()
   end
 
   escapes = [
@@ -23,35 +23,35 @@ defmodule FunWithFlags.UI.HTMLEscape do
   ]
 
   for {match, insert} <- escapes do
-    defp html_safe(<<unquote(match), rest::bits>>, skip, original, acc) do
-      html_safe(rest, skip + 1, original, [acc | unquote(insert)])
+    defp html_escape(<<unquote(match), rest::bits>>, skip, original, acc) do
+      html_escape(rest, skip + 1, original, [acc | unquote(insert)])
     end
   end
 
-  defp html_safe(<<_char, rest::bits>>, skip, original, acc) do
-    html_safe(rest, skip, original, acc, 1)
+  defp html_escape(<<_char, rest::bits>>, skip, original, acc) do
+    html_escape(rest, skip, original, acc, 1)
   end
 
-  defp html_safe(<<>>, _skip, _original, acc) do
+  defp html_escape(<<>>, _skip, _original, acc) do
     acc
   end
 
   for {match, insert} <- escapes do
-    defp html_safe(<<unquote(match), rest::bits>>, skip, original, acc, len) do
+    defp html_escape(<<unquote(match), rest::bits>>, skip, original, acc, len) do
       part = binary_part(original, skip, len)
-      html_safe(rest, skip + len + 1, original, [acc, part | unquote(insert)])
+      html_escape(rest, skip + len + 1, original, [acc, part | unquote(insert)])
     end
   end
 
-  defp html_safe(<<_char, rest::bits>>, skip, original, acc, len) do
-    html_safe(rest, skip, original, acc, len + 1)
+  defp html_escape(<<_char, rest::bits>>, skip, original, acc, len) do
+    html_escape(rest, skip, original, acc, len + 1)
   end
 
-  defp html_safe(<<>>, 0, original, _acc, _len) do
+  defp html_escape(<<>>, 0, original, _acc, _len) do
     original
   end
 
-  defp html_safe(<<>>, skip, original, acc, len) do
+  defp html_escape(<<>>, skip, original, acc, len) do
     [acc | binary_part(original, skip, len)]
   end
 end
